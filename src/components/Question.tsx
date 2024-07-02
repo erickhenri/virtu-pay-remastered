af76ca8fbb246d56022bb17f630173b4ea2d4faf
erickhenri <herick.henri16@gmail.com>
2024-07-02 19:29:32 -0300

    fix arrow in questions

--- src/components/Question.tsx
+++ src/components/Question.tsx
@@ -12,11 +12,13 @@ export function Question({ question, answer }: QuestionProps) {
   return (
     <div className="text-xs sm:text-base">
       <div
-        className="flex cursor-pointer items-center justify-between gap-3 py-4"
+        className="flex cursor-pointer items-center justify-between py-4"
         onClick={() => setChangeAnswer((prevChange) => !prevChange)}
       >
         <span className="font-medium">{question}</span>
-        <CaretDown size={24} />
+        <CaretDown
+          className={`${changeAnswer ? 'rotate-180' : ''} h-auto min-w-4 transition-transform duration-500 sm:min-w-6`}
+        />
       </div>
       <p
         className={`${changeAnswer ? 'mb-4 opacity-100' : 'h-0 scale-0 opacity-0'} transition-opacity duration-500`}
